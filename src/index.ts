0833892120a3851217631c0d1a93d7332a8a44ad
Pumpking <gn131131@sina.com>
2020-10-12 14:13:45 +0800

    切换为技术支持页面

--- src/index.ts
+++ src/index.ts
@@ -14,57 +14,57 @@ import "./vendor/flexble/flexible";
 import "./style.css";
 
 import $ from "jquery";
-import MainFn from "./js/main";
+// import MainFn from "./js/main";
 
-$(document).ready(() => {
-  compatibility();
-  init();
-  hotModuleSet();
-});
+// $(document).ready(() => {
+//   compatibility();
+//   init();
+//   hotModuleSet();
+// });
 
-function init() {
-  const mainFn = new MainFn();
-  mainFn.init();
-}
+// function init() {
+//   const mainFn = new MainFn();
+//   mainFn.init();
+// }
 
-// raf兼容性
-function compatibility() {
-  const win: any = window;
-  let lastTime = 0;
-  const vendors = ["webkit", "moz"];
-  for (let x = 0; x < vendors.length && !win.requestAnimationFrame; ++x) {
-    win.requestAnimationFrame = win[vendors[x] + "RequestAnimationFrame"];
-    win.cancelAnimationFrame =
-      win[vendors[x] + "CancelAnimationFrame"] || // Webkit中此取消方法的名字变了
-      win[vendors[x] + "CancelRequestAnimationFrame"];
-  }
-  if (!win.requestAnimationFrame) {
-    win.requestAnimationFrame = function (callback: any) {
-      const currTime = new Date().getTime();
-      const timeToCall = Math.max(0, 16.7 - (currTime - lastTime));
-      const id = win.setTimeout(function () {
-        callback(currTime + timeToCall);
-      }, timeToCall);
-      lastTime = currTime + timeToCall;
-      return id;
-    };
-  }
-  if (!win.cancelAnimationFrame) {
-    window.cancelAnimationFrame = function (id) {
-      clearTimeout(id);
-    };
-  }
-}
+// // raf兼容性
+// function compatibility() {
+//   const win: any = window;
+//   let lastTime = 0;
+//   const vendors = ["webkit", "moz"];
+//   for (let x = 0; x < vendors.length && !win.requestAnimationFrame; ++x) {
+//     win.requestAnimationFrame = win[vendors[x] + "RequestAnimationFrame"];
+//     win.cancelAnimationFrame =
+//       win[vendors[x] + "CancelAnimationFrame"] || // Webkit中此取消方法的名字变了
+//       win[vendors[x] + "CancelRequestAnimationFrame"];
+//   }
+//   if (!win.requestAnimationFrame) {
+//     win.requestAnimationFrame = function (callback: any) {
+//       const currTime = new Date().getTime();
+//       const timeToCall = Math.max(0, 16.7 - (currTime - lastTime));
+//       const id = win.setTimeout(function () {
+//         callback(currTime + timeToCall);
+//       }, timeToCall);
+//       lastTime = currTime + timeToCall;
+//       return id;
+//     };
+//   }
+//   if (!win.cancelAnimationFrame) {
+//     window.cancelAnimationFrame = function (id) {
+//       clearTimeout(id);
+//     };
+//   }
+// }
 
-function hotModuleSet() {
-  if (module.hot) {
-    module.hot.accept("./js/main.ts", () => {
-      console.log("热部署模块更新");
-      init();
-    });
-  }
+// function hotModuleSet() {
+//   if (module.hot) {
+//     module.hot.accept("./js/main.ts", () => {
+//       console.log("热部署模块更新");
+//       init();
+//     });
+//   }
 
-  if (process.env.NODE_ENV !== "production") {
-    console.log("Looks like we are in development mode!");
-  }
-}
+//   if (process.env.NODE_ENV !== "production") {
+//     console.log("Looks like we are in development mode!");
+//   }
+// }
